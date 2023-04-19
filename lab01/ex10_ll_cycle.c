7254c46b45d29d0ecb52d899ede41f1732978b35
Yjsmall <1186907891@qq.com>
2023-04-19 19:23:45 +0800

    finished all exercises

--- lab01/ex10_ll_cycle.c
+++ lab01/ex10_ll_cycle.c
@@ -2,5 +2,20 @@
 #include "ex10_ll_cycle.h"
 
 int ll_has_cycle(node *head) {
-    /* TODO: Implement ll_has_cycle */
+    if (head == NULL || head->next == NULL) {
+        return 0;
+    }
+
+    node* slow_ptr = head;
+    node* fast_ptr = head->next;
+
+    while (slow_ptr != fast_ptr) {
+        if (fast_ptr == NULL || fast_ptr->next == NULL) {
+            return 0;
+        }
+        slow_ptr = slow_ptr->next;
+        fast_ptr = fast_ptr->next->next;
+    }
+
+    return 1;
 }
